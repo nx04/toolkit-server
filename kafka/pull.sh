8ac66d5a671727a97ac483ce810cc70b9f9437b8
xiaonian <xiaonian0430@163.com>
2020-01-03 17:01:06 +0800

    qewrc

---
+++ kafka/pull.sh
@@ -0,0 +1,2 @@
+docker pull wurstmeister/kafka:2.12-2.4.0
+docker pull zookeeper:3.5.6
--- mysql_cluster/pull.sh
+++ mysql_cluster/pull.sh
@@ -1,5 +1,4 @@
 # Server Latest version:8.0.18
 docker pull mysql:latest
 
-# 或者指定版本
-# docker pull mysql:8.0.18
+docker pull 
