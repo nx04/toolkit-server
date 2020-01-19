0709a383003b6c245a392231f3b10f97f9fe3e5c
xiaonian <xiaonian0430@163.com>
2020-01-19 09:45:17 +0800

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yum install docker
+service docker start
+chkconfig docker on
+docker version
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /bin/docker-compose
+docker-compose --version
