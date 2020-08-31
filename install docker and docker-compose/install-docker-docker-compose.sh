6c89f8c84b8320c82a23565fb63488fb0c7fb4ca
xiaonian <xiaonian0430@163.com>
2020-08-31 15:26:12 +0800

    docker

--- install docker and docker-compose/install-docker-docker-compose.sh
+++ install docker and docker-compose/install-docker-docker-compose.sh
@@ -18,7 +18,7 @@ docker version
 
 
 # install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o "/usr/local/bin/docker-compose"
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o "/usr/local/bin/docker-compose"
 chmod +x /usr/local/bin/docker-compose
 ln -s -f /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
