b19157b0011724e92212f9fd923d239fc43ee562
xiaonian <xiaonian0430@163.com>
2020-09-02 10:49:03 +0800

    121212

--- install docker and docker-compose/install-docker-docker-compose.sh
+++ install docker and docker-compose/install-docker-docker-compose.sh
@@ -4,7 +4,7 @@ yum install -y curl yum-utils
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 #yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 #yum update
-yum install docker-ce docker-ce-cli containerd.io
+yum install -y docker-ce docker-ce-cli containerd.io
 #安装指定版本的 docker 如下
 #yum list docker-ce --showduplicates | sort -r
 #yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io
