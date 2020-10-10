be97deade98e034daf96d031ccc736f70d457efb
xiaonian <xiaonian0430@163.com>
2020-10-10 11:47:05 +0800

    1212

--- install docker and docker-compose/install.sh
+++ install docker and docker-compose/install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+yum remove docker \
+    docker-client \
+    docker-client-latest \
+    docker-common \
+    docker-latest \
+    docker-latest-logrotate \
+    docker-logrotate \
+    docker-engine
+
 yum install -y curl yum-utils
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 #yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-#yum update
 yum install -y docker-ce docker-ce-cli containerd.io
+
 #安装指定版本的 docker 如下
 #yum list docker-ce --showduplicates | sort -r
 #yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io
@@ -19,8 +28,8 @@ docker version
 
 
 # install docker-compose
-# https://github.com/docker/compose/releases/download/1.27.2/docker-compose-$(uname -s)-$(uname -m)
-curl -L "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-infobird/225ee520-f637-11ea-8a36-ebb87efcf8c0.2" -o "/usr/local/bin/docker-compose"
+# https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)
+curl -L "https://696e-infobird-4682b5-1302949103.tcb.qcloud.la/docker-compose/docker-compose-Linux-x86_64-1.27.4" -o "/usr/local/bin/docker-compose"
 chmod +x /usr/local/bin/docker-compose
 ln -s -f /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
