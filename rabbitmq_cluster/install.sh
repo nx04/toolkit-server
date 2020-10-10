fb77bad67ccb505865042f5ec9c721e941466a5c
xiaonian <xiaonian0430@163.com>
2020-10-10 17:46:33 +0800

    amqp

---
+++ rabbitmq_cluster/install.sh
@@ -0,0 +1,63 @@
+yum -y install gcc gcc-c++ glibc-devel make kernel-devel m4 ncurses-devel openssl-devel xmlto perl wget gtk2-devel binutils-devel xz
+
+
+# 安装 make
+# http://ftp.gnu.org/gnu/make/make-4.3.tar.gz
+wget http://ftp.gnu.org/gnu/make/make-4.3.tar.gz -O make-4.3.tar.gz
+tar -xzvf make-4.3.tar.gz
+cd make-4.3
+./configure --prefix=/usr/local/make
+make && make install
+mv /usr/bin/make make.bak
+ln -s /usr/local/make/bin/make /usr/bin/make
+
+# 安装 erlang
+# http://erlang.org/download/otp_src_23.1.tar.gz
+wget https://696e-infobird-4682b5-1302949103.tcb.qcloud.la/erlang/otp_src_23.1.tar.gz -O otp_src_23.1.tar.gz
+tar -xzvf otp_src_23.1.tar.gz
+cd otp_src_23.1
+./configure --prefix=/usr/local/erlang --with-ssl --enable-threads --enable-smp-support --enable-kernel-poll --enable-hipe 
+make && make install
+echo 'export PATH=$PATH:/usr/local/erlang/bin' >> /etc/profile
+source /etc/profile
+
+
+# 配置erlang环境
+# vi /etc/profile #在最后添加下文
+# PATH=$PATH:/usr/local/erlang/bin
+# 使环境变量生效
+# source /etc/profile
+# 测试一下是否安装成功,在控制台输入命令erl
+
+# 安装 rabbitmq
+# https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.9/rabbitmq-server-generic-unix-3.8.9.tar.xz
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.9/rabbitmq-server-generic-unix-3.8.9.tar.xz -O rabbitmq-server-3.8.9.tar.xz
+xz -d rabbitmq-server-3.8.9.tar.xz
+tar -xvf rabbitmq-server-3.8.9.tar
+mv ./rabbitmq-server-3.8.9 /usr/local/rabbitmq
+echo 'export PATH=$PATH:/usr/local/rabbitmq/sbin' >> /etc/profile
+source /etc/profile
+# 创建配置目录
+mkdir /etc/rabbitmq
+
+# 安装 rabbitmq_delayed_meaage_exchange
+# 查看插件列表: rabbitmq-plugins list
+# https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v3.8.0/rabbitmq_delayed_message_exchange-3.8.0.ez
+wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v3.8.0/rabbitmq_delayed_message_exchange-3.8.0.ez
+mv rabbitmq_delayed_message_exchange-3.8.0.ez /usr/local/rabbitmq/plugins/
+rabbitmq-plugins enable rabbitmq_delayed_message_exchange
+
+
+#启动：rabbitmq-server start
+# rabbitmq-server -detached
+#netstat -lnpt|grep beam
+#关闭：rabbitmqctl stop
+#状态：rabbitmqctl status
+#启动web管理插件: rabbitmq-plugins enable rabbitmq_management
+#访问：http://127.0.0.1:15672/
+#rabbitmqctl list_users
+#rabbitmqctl add_user xiaonian 123456
+#rabbitmqctl list_user_permissions xiaonian
+#rabbitmqctl set_user_tags xiaonian administrator
+#rabbitmqctl delete_user guest
+
