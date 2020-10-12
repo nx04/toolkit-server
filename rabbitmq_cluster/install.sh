bc135b43328e832d9f2cfe5b8a8234ca2bb75ad4
xiaonian <xiaonian0430@163.com>
2020-10-12 09:43:29 +0800

    rabbitmq 安装

--- rabbitmq_cluster/install.sh
+++ rabbitmq_cluster/install.sh
@@ -1,9 +1,11 @@
+# rabbitmq server 安装
+
+# 安装依赖
 yum -y install gcc gcc-c++ glibc-devel make kernel-devel m4 ncurses-devel openssl-devel xmlto perl wget gtk2-devel binutils-devel xz
 
-
-# 安装 make
+# 安装 make 4.3
 # http://ftp.gnu.org/gnu/make/make-4.3.tar.gz
-wget http://ftp.gnu.org/gnu/make/make-4.3.tar.gz -O make-4.3.tar.gz
+wget https://696e-infobird-4682b5-1302949103.tcb.qcloud.la/make/make-4.3.tar.gz -O make-4.3.tar.gz
 tar -xzvf make-4.3.tar.gz
 cd make-4.3
 ./configure --prefix=/usr/local/make
@@ -21,17 +23,9 @@ make && make install
 echo 'export PATH=$PATH:/usr/local/erlang/bin' >> /etc/profile
 source /etc/profile
 
-
-# 配置erlang环境
-# vi /etc/profile #在最后添加下文
-# PATH=$PATH:/usr/local/erlang/bin
-# 使环境变量生效
-# source /etc/profile
-# 测试一下是否安装成功,在控制台输入命令erl
-
 # 安装 rabbitmq
 # https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.9/rabbitmq-server-generic-unix-3.8.9.tar.xz
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.9/rabbitmq-server-generic-unix-3.8.9.tar.xz -O rabbitmq-server-3.8.9.tar.xz
+wget https://696e-infobird-4682b5-1302949103.tcb.qcloud.la/rabbitmq/rabbitmq-server-generic-unix-3.8.9.tar.xz -O rabbitmq-server-3.8.9.tar.xz
 xz -d rabbitmq-server-3.8.9.tar.xz
 tar -xvf rabbitmq-server-3.8.9.tar
 mv ./rabbitmq-server-3.8.9 /usr/local/rabbitmq
@@ -40,24 +34,31 @@ source /etc/profile
 # 创建配置目录
 mkdir /etc/rabbitmq
 
-# 安装 rabbitmq_delayed_meaage_exchange
-# 查看插件列表: rabbitmq-plugins list
+# 安装插件 rabbitmq_delayed_meaage_exchange
 # https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v3.8.0/rabbitmq_delayed_message_exchange-3.8.0.ez
-wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v3.8.0/rabbitmq_delayed_message_exchange-3.8.0.ez
+wget https://696e-infobird-4682b5-1302949103.tcb.qcloud.la/rabbitmq/rabbitmq_delayed_message_exchange-3.8.0.ez
 mv rabbitmq_delayed_message_exchange-3.8.0.ez /usr/local/rabbitmq/plugins/
+# 启动delayed_message插件
 rabbitmq-plugins enable rabbitmq_delayed_message_exchange
 
+# 启动web管理插件: 
+rabbitmq-plugins enable rabbitmq_management
 
-#启动：rabbitmq-server start
-# rabbitmq-server -detached
-#netstat -lnpt|grep beam
-#关闭：rabbitmqctl stop
-#状态：rabbitmqctl status
-#启动web管理插件: rabbitmq-plugins enable rabbitmq_management
-#访问：http://127.0.0.1:15672/
-#rabbitmqctl list_users
-#rabbitmqctl add_user xiaonian 123456
-#rabbitmqctl list_user_permissions xiaonian
-#rabbitmqctl set_user_tags xiaonian administrator
-#rabbitmqctl delete_user guest
+# 查看插件列表: 
+rabbitmq-plugins list
+
+
+# 启动server：rabbitmq-server start
+# 守护进程方式启动server：rabbitmq-server -detached
+# 查看进程：netstat -lnpt|grep beam
+# 关闭：rabbitmqctl stop
+# 查看状态：rabbitmqctl status
+
+# 访问地址：http://IP:15672/
+# 查看管理端用户列表：rabbitmqctl list_users
+# 添加管理端用户：rabbitmqctl add_user xiaonian 123456
+# 将 xiaonian 设置为管理员权限：rabbitmqctl set_user_tags xiaonian administrator
+# 删除gust用户：rabbitmqctl delete_user guest
+# 将 xiaonian 设置为远端登录：rabbitmqctl  set_permissions -p / xiaonian '.*' '.*' '.*' 
+# 查看管理端用户权限：rabbitmqctl list_user_permissions xiaonian
 
