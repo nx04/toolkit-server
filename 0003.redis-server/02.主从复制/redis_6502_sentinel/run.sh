ce434d39b72332d96f793022998c7fa435a7d4ab
xiaonian0430 <xiaonian0430@163.com>
2021-11-18 09:27:08 +0800

    搭建主從複製

---
+++ 0003.redis-server/02.主从复制/redis_6502_sentinel/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo $#
+echo $2
+echo $1
+
+# 启动
+if [ "$1"  = "start" ]
+then
+    # 优化内核
+    echo 1 > /proc/sys/vm/overcommit_memory
+    echo 50000 > /proc/sys/net/core/somaxconn
+    redis-server ../sentinel.conf --daemonize yes --port 6502 --sentinel monitor mymaster 116.85.36.13 6379 2
+fi
+
+## 停止服务
+if [ "$1"  = "stop" ]
+then
+    redis-cli -h 127.0.0.1 -p 6502 shutdown
+fi
+
+## 查看状态
+if [ "$1"  = "status" ]
+then
+    redis-cli -h 127.0.0.1 -p 6502 info
+fi
