209d5869bc8abe101cc8ddd0d3ee56b5d4857383
xiaonian0430 <xiaonian0430@163.com>
2021-11-19 16:18:00 +0800

    内核優化

---
+++ 0000.centos/op.sh
@@ -0,0 +1,8 @@
+
+# 优化系统内核参数，单机1万并发，每个连接请求10万次
+ulimit -n 65535
+echo 1 > /proc/sys/vm/overcommit_memory
+echo 50000 > /proc/sys/net/core/somaxconn
+echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle
+echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
+echo 0 > /proc/sys/net/ipv4/tcp_syncookies
