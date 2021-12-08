c36904cafc48294fe55425e66a99f6e22dae0ec8
xiaonian0430 <xiaonian0430@163.com>
2021-12-08 15:50:46 +0800

    Update install.sh

--- 0004.python/install.sh
+++ 0004.python/install.sh
@@ -10,32 +10,31 @@ yum -y install libsndfile readline-devel xz-devel tk-devel gdbm-devel
 
 # openssl
 # https://www.openssl.org/source/openssl-1.1.1l.tar.gz
-wget https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f21b85c6-6337-4b61-b6e7-aca75841afed/22a4ac75-e6b5-4444-8c87-cc83cbf6f19b.gz -O openssl1release.tar.gz
-rm -rf openssl1release && mkdir -p openssl1release
-tar -zxvf openssl1release.tar.gz -C ./openssl1release --strip-components 1
-cd openssl1release
-./config --prefix /usr/local/openssl1release
+wget https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f21b85c6-6337-4b61-b6e7-aca75841afed/22a4ac75-e6b5-4444-8c87-cc83cbf6f19b.gz -O openssl-release.tar.gz
+rm -rf openssl-release && mkdir -p openssl-release
+tar -zxvf openssl-release.tar.gz -C ./openssl-release --strip-components 1
+cd openssl-release
+./config --prefix /usr/local/openssl
 ./config -t
 make && make install
 cd ../
-rm -rf openssl1release openssl1release.tar.gz
-ln -s /usr/local/openssl1release/bin/openssl /usr/bin/openssl
-ln -s /usr/local/openssl1release/include/openssl /usr/include/openssl
-echo "/usr/local/openssl1release/lib" >> /etc/ld.so.conf
-openssl version
+rm -rf openssl-release openssl-release.tar.gz
+ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
+ln -s /usr/local/openssl/include/openssl /usr/include/openssl
+echo "/usr/local/openssl/lib">> /etc/ld.so.conf
 
 # install
 # https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
-wget https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f21b85c6-6337-4b61-b6e7-aca75841afed/55260cf4-9e3d-45cf-90e9-bac2912f3709.tgz -O python3release.tgz
-rm -rf python3release && mkdir -p python3release
-tar -zxvf python3release.tgz -C ./python3release --strip-components 1
-cd python3release
-./configure --prefix /usr/local/python3release --with-openssl=/usr/local/openssl1release
+wget https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f21b85c6-6337-4b61-b6e7-aca75841afed/55260cf4-9e3d-45cf-90e9-bac2912f3709.tgz -O python3-release.tgz
+rm -rf python3-release && mkdir -p python3-release
+tar -zxvf python3-release.tgz -C ./python3-release --strip-components 1
+cd python3-release
+./configure --prefix /usr/local/python3-release --with-openssl=/usr/local/openssl-release
 make && make install
-/usr/local/python3release/bin/python3 -m pip install -i https://mirrors.aliyun.com/pypi/simple/ --upgrade pip
-/usr/local/python3release/bin/pip3 install -i https://mirrors.aliyun.com/pypi/simple/ virtualenv
-ln -s -f /usr/local/python3release/bin/python3 /usr/bin/python3
-ln -s -f /usr/local/python3release/bin/pip3 /usr/bin/pip3
-ln -s -f /usr/local/python3release/bin/virtualenv /usr/bin/virtualenv3
+/usr/local/python3-release/bin/python3 -m pip install -i https://mirrors.aliyun.com/pypi/simple/ --upgrade pip
+/usr/local/python3-release/bin/pip3 install -i https://mirrors.aliyun.com/pypi/simple/ virtualenv
+ln -s -f /usr/local/python3-release/bin/python3 /usr/bin/python3
+ln -s -f /usr/local/python3-release/bin/pip3 /usr/bin/pip3
+ln -s -f /usr/local/python3-release/bin/virtualenv /usr/bin/virtualenv3
 cd ../
-rm -rf python3release python3release.tgz
+rm -rf python3-release python3-release.tgz
