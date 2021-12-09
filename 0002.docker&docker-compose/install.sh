3e8f29187e92c9a108382d0301f29fb887734d3c
xiaonian0430 <xiaonian0430@163.com>
2021-12-09 11:29:31 +0800

    nginx openssl

--- 0002.docker&docker-compose/install.sh
+++ 0002.docker&docker-compose/install.sh
@@ -2,7 +2,25 @@
 ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone
 
 # 编译器和工具
-yum install -y gcc gcc-c++ make cmake autoconf wget tar curl yum-utils git zlib zlib-devel openssl openssl-devel
+yum install -y gcc gcc-c++ make cmake autoconf wget tar curl yum-utils git zlib zlib-devel
+
+# openssl
+# https://www.openssl.org/source/openssl-1.1.1l.tar.gz
+wget https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f21b85c6-6337-4b61-b6e7-aca75841afed/22a4ac75-e6b5-4444-8c87-cc83cbf6f19b.gz -O openssl-release.tar.gz
+rm -rf openssl-release && mkdir -p openssl-release
+tar -zxvf openssl-release.tar.gz -C ./openssl-release --strip-components 1
+cd openssl-release
+./config --prefix=/usr/local/openssl-release
+./config -t
+make
+make install
+cd ../
+rm -rf openssl-release openssl-release.tar.gz
+ln -s -f /usr/local/openssl-release/bin/openssl /usr/bin/openssl
+ln -s -f /usr/local/openssl-release/include/openssl /usr/include/openssl
+echo "/usr/local/openssl-release/lib">> /etc/ld.so.conf
+ldconfig
+openssl version
 
 # install docker
 yum remove docker* -y
