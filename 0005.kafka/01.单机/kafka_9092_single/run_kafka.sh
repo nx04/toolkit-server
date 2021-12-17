0ae26d908ffc2a88bbf20f4020d9e0ae5faa8c68
xiaonian0430 <xiaonian0430@163.com>
2021-12-17 11:09:53 +0800

    kafka

--- 0005.kafka/01.单机/kafka_9092_single/run_kafka.sh
+++ 0005.kafka/01.单机/kafka_9092_single/run_kafka.sh
@@ -3,22 +3,25 @@ echo $#
 echo $2
 echo $1
 
+kafka_release="/usr/local/kafka-release"
+
 # 启动
 if [ "$1"  = "start" ]
 then
     # 优化内核
     cp -rf ../server.properties ./server.properties
-    kafka-server-start.sh server.properties
+    rm -rf kafka-logs
+    $kafka_release/bin/kafka-server-start.sh server.properties
 fi
 
 ## 停止服务
 if [ "$1"  = "stop" ]
 then
-   kafka-server-stop.sh
+  $kafka_release/bin/kafka-server-stop.sh
 fi
 
 ## 查看状态
 if [ "$1"  = "status" ]
 then
-    kafka-server-shell.sh
+    $kafka_release/bin/kafka-server-shell.sh
 fi
--- 0005.kafka/01.单机/kafka_9092_single/run_zookeeper.sh
+++ 0005.kafka/01.单机/kafka_9092_single/run_zookeeper.sh
@@ -3,22 +3,24 @@ echo $#
 echo $2
 echo $1
 
+kafka_release="/usr/local/kafka-release"
+
 # 启动
 if [ "$1"  = "start" ]
 then
     # 优化内核
     cp -rf ../zookeeper.properties ./zookeeper.properties
-    zookeeper-server-start.sh zookeeper.properties
+    $kafka_release/bin/zookeeper-server-start.sh zookeeper.properties
 fi
 
 ## 停止服务
 if [ "$1"  = "stop" ]
 then
-   zookeeper-server-stop.sh
+   $kafka_release/bin/zookeeper-server-stop.sh
 fi
 
 ## 查看状态
 if [ "$1"  = "status" ]
 then
-    zookeeper-server-shell.sh
+    $kafka_release/bin/zookeeper-server-shell.sh
 fi
