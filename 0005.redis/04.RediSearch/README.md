2a385dfa6993904eae247ac12db81824476d32d0
xiaonian0430 <xiaonian0430@163.com>
2021-12-22 17:24:20 +0800

    優化安裝

--- 0005.redis/04.RediSearch/README.md
+++ 0005.redis/04.RediSearch/README.md
@@ -4,6 +4,8 @@
 
 RediSearch is a source available Secondary Index, Query Engine and Full-Text Search over Redis, developed by Redis .
 
+RediSearch 是一个高性能的全文搜索引擎，可作为一个 Redis Module 运行在 Redis 上，是由 RedisLabs 团队开发的。
+
 ## 裸机
 
 ```
--- 0005.redis/04.RediSearch/install.sh
+++ 0005.redis/04.RediSearch/install.sh
@@ -1,14 +1,31 @@
+# http://ftp.gnu.org/gnu/make/make-4.3.tar.gz
+wget https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f21b85c6-6337-4b61-b6e7-aca75841afed/63fbe139-c2d7-4600-9140-93a1e6575524.gz -O make-release.tar.gz
+rm -rf make-release && mkdir -p make-release
+tar -zxvf make-release.tar.gz -C ./make-release --strip-components 1
+cd make-release
+./configure --prefix=/usr/local/make-release
+make
+make install
+mv /usr/bin/make /usr/bin/make.bak
+ln -s -f /usr/local/make-release/bin/make /usr/bin/make
+make -v
+cd ../
+rm -rf make-release make-release.tar.gz
+
 # install RediSearch
 # https://github.com/RediSearch/RediSearch/archive/refs/tags/v2.0.15.tar.gz
-wget https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f21b85c6-6337-4b61-b6e7-aca75841afed/dec435ad-73af-4a80-970b-680c2b238ac3.gz -O rediSearch-release.tar.gz
+# https://gitee.com/xiaonian0430/RediSearch.git
+git clone --recursive https://gitee.com/xiaonian0430/RediSearch.git rediSearch-release
+cd rediSearch-release
+make setup
+make build
+make run
 rm -rf /usr/local/rediSearch-release
 mkdir -p /usr/local/rediSearch-release
-tar -zxvf rediSearch-release.tar.gz -C /usr/local/rediSearch-release --strip-components 1
-rm -rf rediSearch-release.tar.gz
-cd /usr/local/rediSearch-release
-make all
+mv build/redisearch.so /usr/local/rediSearch-release
+rm -rf rediSearch-release
 
 # 修改 redis.conf
 # vim redis.conf
 # 文件底部加入,保存
-# loadmodule   /usr/local/rediSearch-release/src/redisearch.so
+# loadmodule   /usr/local/rediSearch-release/redisearch.so
