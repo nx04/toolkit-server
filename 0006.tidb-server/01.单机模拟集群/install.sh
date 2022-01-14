c0dd53a33abc55a5dbf2b526872cd75cc4f156ea
xiaonian0430 <xiaonian0430@163.com>
2022-01-14 10:13:21 +0800

    tidb 集群部署

--- 0006.tidb-server/01.单机模拟集群/install.sh
+++ 0006.tidb-server/01.单机模拟集群/install.sh
@@ -1,1 +1,7 @@
+# 安装 TiUP
+# curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
+source /root/.bash_profile
+tiup cluster
+tiup update --self && tiup update cluster
+
 tiup cluster check ./topology.yaml --user root -p
