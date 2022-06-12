b9008db91b7890efe89d0a3fca92d39d70e4043a
肖念 <xiaonian0430@163.com>
2022-06-12 23:17:35 +0800

    gitea 部署

--- 10202.Gitea/01.docker-deploy/run.sh
+++ 10202.Gitea/01.docker-deploy/run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-useradd xiaonian
-groupadd softgroup
-gpasswd -a xiaonian softgroup
+
+# 创建docker网络
+create_network_event(){
+    filter_name=`docker network ls | grep docker_network | awk '{ print $2 }'`
+    if [ "$filter_name" == "" ]; then
+        #不存在就创建
+        docker network create --subnet 172.19.0.0/16 --gateway 172.19.0.1 docker_network
+        echo 'docker_network created [\e[32mok\e[0m]'
+    else
+        echo -e 'docker_network [\e[32mok\e[0m]'
+    fi
+}
+create_network_event
+
+mkdir -p /data/gitea_server_001
 docker-compose up -d
+
+
