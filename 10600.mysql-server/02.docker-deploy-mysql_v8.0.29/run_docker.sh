4cd8ec316dd3e419eced8a05135ede6a68982169
肖念 <xiaonian0430@163.com>
2022-06-12 22:12:37 +0800

    12

--- 10600.mysql-server/02.docker-deploy-mysql_v8.0.29/run_docker.sh
+++ 10600.mysql-server/02.docker-deploy-mysql_v8.0.29/run_docker.sh
@@ -92,7 +92,7 @@ mysql_server_run(){
 mysql_server_start_event(){
     mkdir -p /data/$mysql_server_name/conf
     cp -rf $mysql_conf /data/$mysql_server_name/conf
-    docker run -p 53306:3306 -p 13300:33060 --restart=always --net=docker_network --ip=172.19.0.10 --name $mysql_server_name -v /data/$mysql_server_name/data:/var/lib/mysql -v /data/$mysql_server_name/conf/mysqld.cnf:/etc/mysql/mysql.conf.d/mysqld.cnf -e MYSQL_ROOT_PASSWORD=xn9981k% -d $mysql_image_name
+    docker run -p 53306:3306 -p 13300:33060 --restart=always --net=docker_network --ip=172.19.0.10 --name $mysql_server_name -v /data/$mysql_server_name/data:/var/lib/mysql -v /data/$mysql_server_name/conf/my.cnf:/etc/mysql/my.cnf -e MYSQL_ROOT_PASSWORD=xn9981k% -d $mysql_image_name
 }
 mysql_server_reload_event(){
     docker restart $mysql_server_name
