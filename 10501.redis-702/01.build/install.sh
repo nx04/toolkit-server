1b2cc5bab065a030f5f7475def1a91e40683cdbb
肖念 <xiaonian0430@163.com>
2022-07-11 15:42:02 +0800

    redis 702

---
+++ 10501.redis-702/01.build/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##########安装 redis7############
+# 时区
+ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone
+
+# 编译器和工具
+yum install -y yum-utils
+yum install -y gcc gcc-c++ make automake autoconf
+yum install -y lsof net-tools sysstat tree iotop
+yum install -y wget tar curl git unzip zip
+
+# 第三方的开发包
+yum -y install libxml2 libxml2-devel sqlite-devel libcurl-devel libevent-devel openssl openssl-devel
+
+# redis702
+# https://github.com/redis/redis/archive/refs/tags/7.0.2.tar.gz
+wget https://696e-infobird-4682b5-1302949103.tcb.qcloud.la/server/redis-7.0.2.tar.gz -O redis-release.tar.gz --no-check-certificate
+rm -rf redis-release /usr/local/redis-release
+mkdir -p redis-release
+tar -zxvf redis-release.tar.gz -C ./redis-release --strip-components 1
+cd redis-release
+make && make install PREFIX=/usr/local/redis-release
+cd ../ && rm -rf redis-release redis-release.tar.gz
+ln -s -f /usr/local/redis-release/bin/redis-server /usr/bin/redis-server
+ln -s -f /usr/local/redis-release/bin/redis-cli /usr/bin/redis-cli
+ln -s -f /usr/local/redis-release/bin/redis-sentinel /usr/bin/redis-sentinel
+
+
+
---
+++ 10501.redis-702/02.deploy/run_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 时区
+ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone
+
+
+mkdir -p /data/demo_server_redis_702/config
+mkdir -p /data/demo_server_redis_702/logs
+mkdir -p /data/demo_server_redis_702/www
+cp -rf redis.conf /data/demo_server_redis_702/config
+docker rm -f demo_server_redis_702
+docker run -d -p 56379:6379 --name demo_server_redis_702 -v /data/demo_server_redis_702/config/redis.conf:/data/server/conf/redis.conf redis:7.0.2
+
