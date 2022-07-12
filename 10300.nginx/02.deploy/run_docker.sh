48f0f63ce85a6bf73f5a492b0dd96e18239e2577
肖念 <xiaonian0430@163.com>
2022-07-12 23:38:17 +0800

    11212

--- 10300.nginx/02.deploy/run_docker.sh
+++ 10300.nginx/02.deploy/run_docker.sh
@@ -8,5 +8,5 @@ mkdir -p /data/demo_server_nginx/www
 mkdir -p /data/demo_server_nginx/config
 cp -rf demo.com.conf /data/demo_server_nginx/config
 docker rm -f demo_server_nginx
-docker run -d -p 58800:80 --name demo_server_nginx -v /data/demo_server_nginx/www:/data/server/www -v /data/demo_server_nginx/config/demo.com.conf:/usr/local/nginx-release/conf.d/demo.com.conf nginx:1.22.0
+docker run -d -p 58800:80 --name demo_server_nginx -v /data/demo_server_nginx/config/nginx.conf:/usr/local/nginx-release/conf/nginx.conf -v /data/demo_server_nginx/www:/data/server/www -v /data/demo_server_nginx/config/demo.com.conf:/usr/local/nginx-release/conf.d/demo.com.conf nginx:1.22.0
 
