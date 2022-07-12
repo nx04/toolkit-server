ca5c6e4913aaf7e4af865d1715cf8b62f5611aa4
肖念 <xiaonian0430@163.com>
2022-07-12 22:50:19 +0800

    nginx 构建和部署

---
+++ 10300.nginx/02.deploy/README.md
@@ -0,0 +1,10 @@
+# nginx 部署
+
+## 相关命令
+```
+sh run_docker.sh
+
+# docker-compose up -d
+```
+
+
---
+++ 10300.nginx/02.deploy/run_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 时区
+ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone
+
+# 初次创建服务
+mkdir -p /data/demo_server_nginx/www
+mkdir -p /data/demo_server_nginx/config
+cp -rf demo.com.conf /data/demo_server_nginx/config
+docker rm -f demo_server_nginx
+docker run -d -p 58800:80 --name demo_server_nginx -v /data/demo_server_nginx/www:/data/server/www -v /data/demo_server_nginx/config/demo.com.conf:/usr/local/nginx-release/conf.d/demo.com.conf nginx:1.22.0
+
