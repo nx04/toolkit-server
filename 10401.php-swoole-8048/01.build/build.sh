c92fc2ce2c67df0becab96f43a023a128184e340
肖念 <xiaonian0430@163.com>
2022-07-13 11:44:49 +0800

    1212

--- 10401.php-swoole-8048/01.build/build.sh
+++ 10401.php-swoole-8048/01.build/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# 构建 docker 镜像
+# 构建 docker 镜像2022
 docker build -t="php-swoole-8048:2.0.0" .
--- 10401.php-swoole-8048/01.build/install.sh
+++ 10401.php-swoole-8048/01.build/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##########安装 php############
+##########安装 php8############
 # 时区
 ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone
 
