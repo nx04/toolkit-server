4d811fa32b4b174998aa6de8f21b7d1be06cad1b
肖念 <xiaonian0430@163.com>
2022-07-13 23:55:30 +0800

    sw

---
+++ 10401.php-swoole-8048/08.deploy_gateway copy/README.md
@@ -0,0 +1,15 @@
+# php-swoole-gateway 部署
+
+
+## 代码仓库
+
+git clone https://github.com/xiaonian0430/demo_server_php_swoole_gateway.git /data/demo_server_php_swoole_gateway/www
+
+## 相关命令
+```
+sh run_docker.sh
+
+# docker-compose up -d
+```
+
+
---
+++ 10401.php-swoole-8048/08.deploy_gateway copy/run_compose.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 创建docker网络
+create_network_event(){
+    filter_name=`docker network ls | grep docker_network | awk '{ print $2 }'`
+    if [ "$filter_name" == "" ]; then
+        # 不存在就创建
+        docker network create --subnet 172.19.0.0/24 --gateway 172.19.0.1 docker_network
+        echo 'docker_network created [\e[32mok\e[0m]'
+    else
+        echo -e 'docker_network [\e[32mok\e[0m]'
+    fi
+}
+create_network_event
+
+##部署##
+mkdir -p /data/demo_server_php_swoole_gateway/www
+docker rm -f demo_server_php_swoole_gateway
+docker rm -f demo_server_php_swoole_worker
+docker rm -f demo_server_php_swoole_register
+docker-compose -f ./docker-compose.yml up -d
+
+
