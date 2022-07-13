b446184bdb89a44dcf35e9319c6f41c611c63370
肖念 <xiaonian0430@163.com>
2022-07-13 13:32:10 +0800

    demo_server_php_swoole_tcp

--- 10401.php-swoole-8048/02.deploy-http/run_docker.sh
+++ 10401.php-swoole-8048/02.deploy-http/run_docker.sh
@@ -6,5 +6,5 @@ ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai"
 # 初次创建服务
 mkdir -p /data/demo_server_php_swoole_http/www
 docker rm -f demo_server_php_swoole_http
-docker run -d -p 59501:9501 --name demo_server_php_swoole_http -v /data/demo_server_php_swoole_http/www:/data/www php-swoole-8048:2.0.0 /bin/bash -c "php http_server.php"
+docker run -d -p 59501:9501 --name demo_server_php_swoole_http -v /data/demo_server_php_swoole_http/www:/data/www php-swoole-8048:2.0.0 /bin/bash -c "php server_http.php"
 
