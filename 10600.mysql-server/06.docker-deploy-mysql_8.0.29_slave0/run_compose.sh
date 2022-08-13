9b99dac4895c609c9907ed3a0336afa03af0adab
肖念 <xiaonian0430@163.com>
2022-08-13 18:09:53 +0800

    多主一从

---
+++ 10600.mysql-server/06.docker-deploy-mysql_8.0.29_slave0/run_compose.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 创建docker网络
+create_network_event(){
+    filter_name=`docker network ls | grep docker_network | awk '{ print $2 }'`
+    if [ "$filter_name" == "" ]; then
+        #不存在就创建
+        docker network create --subnet 172.19.0.0/24 --gateway 172.19.0.1 docker_network
+        echo 'docker_network created [\e[32mok\e[0m]'
+    else
+        echo -e 'docker_network [\e[32mok\e[0m]'
+    fi
+}
+create_network_event
+
+
+mkdir -p /data/mysql_server_006_slave0/conf
+mkdir -p /data/mysql_server_006_slave0/log
+mkdir -p /data/mysql_server_006_slave0/data
+cp -rf ./my.cnf /data/mysql_server_006_slave0/conf/my.cnf
+docker-compose up -d
+
+#ALTER USER 'root'@'%' IDENTIFIED BY '123456' PASSWORD EXPIRE NEVER;
+#ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
+
+
+
