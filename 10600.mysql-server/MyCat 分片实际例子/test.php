66f1e615e8c24ab7badd5b6daa5c93e5bef86f56
nicshaw <xiaonian0430@163.com>
2025-10-15 11:19:43 +0800

    Update test.php

--- 10600.mysql-server/MyCat 分片实际例子/test.php
+++ 10600.mysql-server/MyCat 分片实际例子/test.php
@@ -2,24 +2,20 @@
 
 $pdo = new PDO('mysql:host=192.168.109.129:58066;dbname=mycat_testdb', 'root', '123456');
 
-// 准备SQL语句
-$stmt = $pdo->prepare('INSERT INTO t_user (id, name) VALUES (:value1, :value2)');
-
-// 准备数据
-$data = [];
 $idStart=100000001;
 for ($i = 0; $i < 1000000; $i++) {
     $idStart++;
-    $data[] = [
-        ':value1' =>$idStart,
-        ':value2' => "测试_$idStart"
-    ];
-}
 
-// 分批执行插入
-$chunkSize = 1000; // 每次插入1000条记录
-$count = count($data);
-for ($i = 0; $i < $count; $i += $chunkSize) {
-    $chunk = array_slice($data, $i, $chunkSize);
-    $stmt->execute($chunk);
+    $sql = "INSERT INTO t_user (id, name) VALUES (:value1, :value2)";
+    $stmt = $pdo->prepare($sql);
+
+    // 绑定参数并执行
+    $stmt->bindParam(':value1', $value1);
+    $stmt->bindParam(':value2', $value2);
+
+    $value1 = $idStart;
+    $value2 = "测试_$idStart";
+    $stmt->execute();
+
+    echo $idStart.PHP_EOL;
 }
