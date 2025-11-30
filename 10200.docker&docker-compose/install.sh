454e7d9a3a284c9b6b3a8b8c1fc484ac5f9a5e69
abc <xiaonian0430@163.com>
2025-11-30 12:28:36 +0800

    Update install.sh

--- 10200.docker&docker-compose/install.sh
+++ 10200.docker&docker-compose/install.sh
@@ -6,8 +6,7 @@ ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai"
 # build tools
 sudo yum update -y
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 
 # docker
 yum remove docker* -y
